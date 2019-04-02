f24fbce3822f8772f00685919b48be47132d05b2
HarryBatchelor <batchelorh@cardiff.ac.uk>
2019-04-02 15:02:25 +0100

    almost done

--- Recommend.py
+++ Recommend.py
@@ -1,6 +1,8 @@
 import numpy as np
 from collections import Counter
 import matplotlib
+import math
+global table
 
 raw_history = open('history.txt','r')
 raw_queries = open('queries.txt','r')
@@ -15,43 +17,63 @@ Number_of_Customers = int(infomation[0])
 Number_of_items = int(infomation[1])
 Number_of_Transactions = int(infomation[2])
 
-if not __debug__:
-	print(infomation)
-	print(history)
-	print(queries)
-	print('\n')
-
-def build_customer_item_history_table():
+def build_table():
+	global table
 	table = []
-	for i in range(Number_of_Customers):
-		table.append([0]*Number_of_items)
+	for i in range(Number_of_Customers+1):
+		table.append([0]*(Number_of_items+1))
 	for case in history:
-		user        = int(case.split(' ')[0])-1
-		item_bought = int(case.split(' ')[1])-1
-		table[user][item_bought] = 1
-	print("Positive entries: %s" %find_posistive_entries(table))
-	show_table(table)
+		user = int(case.split(' ')[0])
+		item_bought = int(case.split(' ')[1])
+		table[item_bought][user] = 1
+	print("Positive entries: %s" %Counter(str(table))['1'])
+	print('Average angle: %s'%Average_angle())
 
-def show_table(t):
-	print('		Item:')
-	formatting = '      '
-	for i in range(0,Number_of_items):
-		formatting += ('  '+str(i+1))
-	print(formatting)
-	for i in range(len(t)):
-		print('User %s %s' %(i+1,t[i]))
-	print()
+def Average_angle():
+	all_pairs = []
+	for ix in range(1, Number_of_items+1):
+		for iy in range(1,Number_of_items+1):
+			if ix!=iy:
+				all_pairs.append(calculate_angle(table[ix],table[iy]))
+	return round(np.mean(all_pairs),2)
 
-def find_posistive_entries(t):
-	return (Counter(str(t))['1'])
+def calculate_angle(x, y):
+	norm_x = np.linalg.norm(x)
+	norm_y = np.linalg.norm(y)
+	return round(math.degrees(math.acos(np.dot(x,y) / (norm_x * norm_y))),2)
+
+def check_angle(item,cart):
+	all_angles = []
+	if item !=' ':
+		for i in range(1,6):
+			if str(i) not in cart:
+				all_angles.append([i,(calculate_angle(table[int(item)], table[i]))])
+	return sorted(all_angles,key=lambda x: x[1])[0]
+
+def order(matches):
+	ordered_matches = sorted(matches, key=lambda x: x[0])
+	out_string= ''
+	for entries in ordered_matches:
+		out_string += str(entries[1])+''
+	return(out_string)
 
 def main():
-	build_customer_item_history_table()
-	print('Average angle:  ')
+	build_table()
 	for cart in queries:
+		matches_found=[]
+		matches_please_dont_look_at_this=[]
 		print("Shopping Cart: %s" %cart)
 		items_in_cart = cart.split(' ')
 		for item in items_in_cart:
-			print('Item: %s match: 0 angle: 0'%item)
-		print()
+			match, angle = check_angle(item,list(cart))
+			if angle > 90:
+				continue
+			elif match not in matches_please_dont_look_at_this:
+				matches_found.append([angle,match])
+				matches_please_dont_look_at_this.append(match)
+			if angle != 90:
+				print('Item: %s; match: %s, angle: %s'%(item,match,angle))
+			else:
+				print('Item: %s no match'%item)
+		print('Recommend: '+order(matches_found))
 main()
